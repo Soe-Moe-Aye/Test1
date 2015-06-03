f66171f087ccc0772f4034ed1cd3929957c91b5a
Soe Moe Aye <Soe Moe Aye@194.47.32.37>
2015-06-03 14:14:34 +0200

    New Test1

---
+++ src/test1/Test1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author Soe Moe Aye
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
