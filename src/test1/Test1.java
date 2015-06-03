b876346f19bc9704f0cff6db5f6325aafbaa1635
Soe Moe Aye <Soe-Moe-Aye@users.noreply.github.com>
2015-06-03 14:40:08 +0200

    Update Test1.java

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -14,6 +14,6 @@ public class Test1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, how are you today?");
     }
 }
